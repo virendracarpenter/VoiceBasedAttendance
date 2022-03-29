881bee40af8383e007855fd7591a1e91067ac0b2
Virendra Carpenter <viren.28opensky@gmail.com>
2022-03-29 21:02:38 +0530

    inital commit

--- vba.py
+++ vba.py
@@ -1,60 +1,100 @@
 from gtts import gTTS 
 import speech_recognition as sr
-
+import mysql.connector
 #from playsound import playsound # uncomment to use in Windows
-import os 
+import os
 
 # Path /home/virendra/Projects/python/project-vba/
+r = sr.Recognizer()
 path = "hello.mp3"
 
 def speak(input):
     myobj = gTTS(text=input, lang='en', slow=False)
-
+    
     # Saving the converted audio in a mp3 file named
     save = path
     myobj.save(save)
-    
-    # Playing the converted file
+
     # Uncomment for Windowse
     # playsound()
 
     #For linux
     os.system("mpg123 " + save)
 
+def attandance(input):
+    print('Input String : ' + input)
 
-#speak('Hello, How are you, welcome Virendra')
+    present = False
 
-#Initiаlize  reсоgnizer  сlаss  (fоr  reсоgnizing  the  sрeeсh)
-r = sr.Recognizer()
+    if "present" or "yes" in input.split():
+        #speak('Student is Prensent')
+        present = True
+        print("present")
+        print(present)
+    else:        
+        print(present)
+        print("Absent")
 
-#while(1):    
-    # Exception handling to handle
-    # exceptions at the runtime
-#    try:   
-        # use the microphone as source for input.
-        
-with sr.Microphone() as source:
-    
-    # wait for a second to let the recognizer
-    # adjust the energy threshold based on
-    # the surrounding noise level
-    r.adjust_for_ambient_noise(source, duration=0.2)
-             
-    # listens for the user's input
-    #audio = r.listen(source)
-    print("Recognizing...")
-    audio = r.record(source, duration = 5)
+def getInput():
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source, duration=0.2)
+        print("Recognizing...")
+        audio = r.record(source, duration = 5)
+        print("done")
     try:
-            
-        # Using google to recognize audio
+        
         MyText = r.recognize_google(audio)
         MyText = MyText.lower()
-            
-        print("Did you say " + MyText)
-        speak(MyText)
-             
-    except sr.RequestError as e:
-        print("Could not request results; {0}".format(e))
-         
+        #print("Did you say " + MyText)
+        
+        attandance(MyText)
+
+        #return MyText
+
+    # except sr.RequestError as e:
+    #     speak("Could not request results; {0}".format(e))
     except sr.UnknownValueError:
-       print("unknown error occured")
+        speak("unknown error occured")
+
+# Runner code
+
+getInput()
+
+#details
+
+# get name of the  student from db
+
+# put present OR absent in repective coloumn
+
+# next name
+
+
+# dataBase = mysql.connector.connect(
+#   host="localhost",
+#   user="root",
+#   password="123qwe1q2w3e",
+# )
+
+# print(dataBase)
+
+ 
+# # preparing a cursor object
+# cursorObject = dataBase.cursor()
+
+# # creating database
+# cursorObject.execute("CREATE DATABASE studentRecord")
+
+# # creating table
+# studentRecord = """CREATE TABLE STUDENT (
+#                    NAME  VARCHAR(20) NOT NULL,
+#                    BRANCH VARCHAR(50),
+#                    ROLL INT NOT NULL,
+#                    SECTION VARCHAR(5),
+#                    AGE INT
+#                    )"""
+  
+# # table created
+# cursorObject.execute(studentRecord)
+
+# # disconnecting from server
+# dataBase.close()
