4ca0098546d110b1bd6298904870a9de5f8c76f4
coderpradyumn <101621036+coderpradyumn@users.noreply.github.com>
2022-04-20 11:38:32 +0530

    intial commit

--- vba.py
+++ vba.py
@@ -1,100 +1,99 @@
-from gtts import gTTS 
+import pandas as pd
+import pyttsx3
 import speech_recognition as sr
-import mysql.connector
-#from playsound import playsound # uncomment to use in Windows
-import os
 
-# Path /home/virendra/Projects/python/project-vba/
+
 r = sr.Recognizer()
-path = "hello.mp3"
-
-def speak(input):
-    myobj = gTTS(text=input, lang='en', slow=False)
-    
-    # Saving the converted audio in a mp3 file named
-    save = path
-    myobj.save(save)
-
-    # Uncomment for Windowse
-    # playsound()
-
-    #For linux
-    os.system("mpg123 " + save)
-
-def attandance(input):
-    print('Input String : ' + input)
-
-    present = False
-
-    if "present" or "yes" in input.split():
-        #speak('Student is Prensent')
-        present = True
-        print("present")
-        print(present)
-    else:        
-        print(present)
-        print("Absent")
-
-def getInput():
-    with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source, duration=0.2)
-        print("Recognizing...")
-        audio = r.record(source, duration = 5)
-        print("done")
-    try:
-        
-        MyText = r.recognize_google(audio)
-        MyText = MyText.lower()
-        #print("Did you say " + MyText)
-        
-        attandance(MyText)
-
-        #return MyText
-
-    # except sr.RequestError as e:
-    #     speak("Could not request results; {0}".format(e))
-    except sr.UnknownValueError:
-        speak("unknown error occured")
-
-# Runner code
-
-getInput()
-
-#details
-
-# get name of the  student from db
-
-# put present OR absent in repective coloumn
-
-# next name
+engine = pyttsx3.init()
 
 
-# dataBase = mysql.connector.connect(
-#   host="localhost",
-#   user="root",
-#   password="123qwe1q2w3e",
-# )
+def take_attendence():
+    count = 0
+    df = pd.read_csv(r'attendence.csv')
+    enroll = df['SE Number'].tolist()
+    enrollno = df['Enrollment Number'].tolist()
+    df['Status'] = 0
+    df.to_csv('attendence.csv', index=False)
 
-# print(dataBase)
 
- 
-# # preparing a cursor object
-# cursorObject = dataBase.cursor()
+    for i in range(len(enroll)):
+        engine.setProperty("rate", 180)
+        engine.say("Enrollment number")
+        engine.setProperty("rate", 150)
+        engine.say(enroll[i])
+        engine.runAndWait()
+        k = True
+        attempt = 0
 
-# # creating database
-# cursorObject.execute("CREATE DATABASE studentRecord")
+        while (k and attempt < 3):
+            try:
 
-# # creating table
-# studentRecord = """CREATE TABLE STUDENT (
-#                    NAME  VARCHAR(20) NOT NULL,
-#                    BRANCH VARCHAR(50),
-#                    ROLL INT NOT NULL,
-#                    SECTION VARCHAR(5),
-#                    AGE INT
-#                    )"""
-  
-# # table created
-# cursorObject.execute(studentRecord)
+                with sr.Microphone() as source:
 
-# # disconnecting from server
-# dataBase.close()
+                    engine.setProperty("rate", 120)
+                    engine.say("Speak Now")
+                    engine.runAndWait()
+
+                    print("Recognizing...", end='  ')
+                    print(enrollno[i])
+
+                    audio_data = r.record(source, duration=3)
+                    text = r.recognize_google(audio_data)
+                    print(text)
+
+                    if text == "present" or text == "yes":
+                        df.loc[i, 'Status'] = 1
+                        df.to_csv("attendence.csv", index=False)
+                        k = False
+                        count += 1
+                    elif text == "absent" or text == "no":
+                        # df.loc[i, 'Status'] = 0
+                        k = False
+                    else:
+                        attempt += 1
+                        print("Attempt Left", 3 - attempt)
+            except sr.RequestError as e:
+                print("Could not request results\nInternet Connection Slow; {0}".format(e))
+
+            except sr.UnknownValueError:
+                attempt += 1
+                if attempt < 3:
+                    print("Try Again\nAttempt Left", 3 - attempt)
+    print("Numbers of students present = ", count)
+
+
+
+def add_new():
+    n = int(input("How many student do you want to add: "))
+    for x in range(n):
+        print("~~~~~~~~~~~~~Student ",x+1," Info~~~~~~~~~~~~~")
+        enroll = input("Enrollment No.: ")
+        se = input("SE No.: ")
+        name = input("Name: ")
+        state = input("Status: ")
+        df = pd.read_csv(r'attendence.csv')
+        df2 = pd.DataFrame({'Enrollment Number': [enroll], 'SE Number': [se], 'Name': [name], 'Status': [state]})
+        df = pd.concat([df, df2], ignore_index=True, axis=0)
+        df.to_csv("attendence.csv", index=False)
+
+        df = pd.read_csv(r'attendence.csv')
+        df1 = df.loc[:, df.columns != "Index Number"]
+        sorted_df = df1.sort_values(by=["Name"], ascending=True)
+        sorted_df.to_csv("attendence.csv", index=False)
+
+    df = pd.read_csv(r'attendence.csv')
+    df.reset_index(level=0, inplace=True)
+    df.to_csv("attendence.csv", index=False)
+
+
+while(1):
+    st = int(input("\n\n\n[Type 1 for Add new Student]    AND    [Type 2 for Start Attendance]    AND    [Any Other Number For TErminate Program].\n ===> "))
+    if st == 1:
+        print("\n\n============Adding A New Student================\n\n")
+        add_new()
+
+    elif st == 2:
+        print("\n\n============Start Taking Attendance================\n\n")
+        take_attendence()
+    else:
+        exit()
